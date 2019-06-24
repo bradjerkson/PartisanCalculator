85d02eac6d262cc68f158896fee8f559d3fe739a
Ubuntu <ubuntu@research.novalocal>
2019-06-24 16:23:15 +0000

    fuckery afoot

--- Web/rest_api.py
+++ Web/rest_api.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 
-
-
-import pandas
-import sys, getopt
-
 app = Flask(__name__)
 
 #Only handles GET requests. Effectively a read-only API.
 
 
-@app.route('/')
+@app.route("/")
 def default():
-    return "CouchDB API Server: connection successful"
+    return "API Server: connection successful"
 
 
-@app.route('/receive', methods=['GET', 'POST'])
-def receive(uuid):
-    content = request.get_json()
-    print(content)
-    return uuid
+#@app.route("/receive", methods=['GET', 'POST'])
+#def receive(uuid):
+#    content = request.get_json()
+#    print(content)
+#    return "HI"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+
