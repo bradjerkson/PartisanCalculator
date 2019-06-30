8365debfe15a5b70d7b9849f140383647da64551
Brad <bradley_jackson@ymail.com>
2019-06-30 11:32:29 +1000

    converted analysis functions into a class

--- Model/analysis.py
+++ Model/analysis.py
@@ -6,56 +6,59 @@ import sys
 from collections import Counter
 from sklearn.naive_bayes import GaussianNB
 
-scoring = {
-    'extreme_right':3,
-    'right':2,
-    'right-center':1,
-    #center will move person 1 point towards 0
-    'center':0,
-    'left-center':-1,
-    'left':-2,
-    'extreme_left':-3
-}
-
-def full_view(df):
-    #here we print out every result when printing dataframes
-    pd.set_option('display.max_colwidth', -1)
-    pd.set_option('display.max_rows', len(df))
-    #print(df)
-
-def load_instance(filename):
-    with open(filename) as inputfile:
-        data = json.load(inputfile)
-
-    counts = (Counter(data["urls"]))
-    df = pd.DataFrame.from_dict(counts, orient='index').reset_index()
-    df['index'] = df['index'].str.replace('www[23]?.', '', case=False)
-    return df.sort_values(0, ascending=False)
+class PartisanModel:
+    def __init__(self, labels, input_history):
+        self.input_history = input_history
+        self.labels = labels
+        self.scoring = {
+            'extreme_right':3,
+            'right':2,
+            'right-center':1,
+            #center will move person 1 point towards 0
+            'center':0,
+            'left-center':-1,
+            'left':-2,
+            'extreme_left':-3
+        }
+        self.df = None
+        self.counts = None
 
 
-def generate_user_media_history(df, counts):
-    out = counts[counts['index'].isin(df['source_url_processed'])]
-    df_out = df.loc[df['source_url_processed'].isin(out['index'])]
-    final_out = pd.merge(out, df_out, left_on=['index'], right_on=['source_url_processed'])
-    return final_out
+    def full_view(self, df):
+        #here we print out every result when printing dataframes
+        pd.set_option('display.max_colwidth', -1)
+        pd.set_option('display.max_rows', len(df))
+        print(df)
 
-def simple_classifier(history, scoring):
-    scores = history.apply(lambda x: (x[0]) * scoring[x['bias']], axis=1)
+    def load_instance(self, filename):
+        with open(filename) as inputfile:
+            data = json.load(inputfile)
 
-    #handle the zeroes
-    zeroes = scores[scores == 0].shape[0]
-    
-    #tf-idf?
-    score = (scores.sum() - zeroes) if scores.sum() > 0 else (scores.sum() + zeroes)
-    return score / len(history)
+        counts = (Counter(data["urls"]))
+        df = pd.DataFrame.from_dict(counts, orient='index').reset_index()
+        df['index'] = df['index'].str.replace('www[23]?.', '', case=False)
+        return df.sort_values(0, ascending=False)
 
-def main(input_history):
-    df = pd.read_csv("newsmedia.csv", header=0)
-    counts = load_instance(input_history)
-    full_view(df)
-    history = generate_user_media_history(df, counts)
-    score = simple_classifier(history, scoring)
-    print(score)
 
-if __name__ == "__main__":
-	main(sys.argv[1])
+    def generate_user_media_history(self, df, counts):
+        out = counts[counts['index'].isin(df['source_url_processed'])]
+        df_out = df.loc[df['source_url_processed'].isin(out['index'])]
+        final_out = pd.merge(out, df_out, left_on=['index'], right_on=['source_url_processed'])
+        return final_out
+
+    def simple_classifier(self, history, scoring):
+        scores = history.apply(lambda x: (x[0]) * scoring[x['bias']], axis=1)
+
+        #handle the zeroes
+        zeroes = scores[scores == 0].shape[0]
+        
+        #tf-idf?
+        score = (scores.sum() - zeroes) if scores.sum() > 0 else (scores.sum() + zeroes)
+        return score / len(history)
+
+    def run(self):
+        self.df = pd.read_csv(self.labels, header=0)
+        self.counts = self.load_instance(self.input_history)
+        self.history = self.generate_user_media_history(self.df, self.counts)
+        self.score = self.simple_classifier(self.history, self.scoring)
+        print(self.score)
